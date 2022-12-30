02fd0f0f157a5bc28f524251da696c69f596452d
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2022-12-30 20:01:39 +0700

    Navbar Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import Navbar from "./scenes/navbar";
+
+type Props = {
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
+};
+
 function App() {
-  return <div className="app">app</div>;
+  const [selectedPage, setSelectedPage] = useState<string>("home");
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
